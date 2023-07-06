23897b48b7099679eaa31777a85242da0306b5e5
vinitus <tlsdnrng@gmail.com>
2023-07-06 16:57:51 +0900

    feat:Route 컴포넌트 props에 타입 강화, props를 살펴보기 위한 console.log

--- src/router/Route.tsx
+++ src/router/Route.tsx
@@ -1,3 +1,13 @@
-export default function Route() {
+import React from 'react';
+
+interface RouteProps {
+  path: string;
+  component: React.ReactNode;
+}
+
+export default function Route(props: RouteProps) {
+  const { path, component } = props;
+  console.log(path, component);
+
   return <div>Route</div>;
 }
