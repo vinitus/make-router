d6455cc4c84d83f6292cec5d1dd18728207d7c70
vinitus <tlsdnrng@gmail.com>
2023-07-06 16:34:10 +0900

    feat:createContext

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,3 +1,13 @@
+import { createContext } from 'react';
+
+interface PathnameState {
+  pathname: string;
+}
+
+const pathnameState: PathnameState = { pathname: '' };
+
+const RouterContext = createContext(pathnameState);
+
 export default function Router() {
   return <div>Router</div>;
 }
