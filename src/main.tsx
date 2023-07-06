e51f5489dae59ec373ab3e169ad3ca72b2154108
vinitus <tlsdnrng@gmail.com>
2023-07-06 16:59:28 +0900

    feat:Route, Root, About 렌더링을 위한 jsx 구문 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './router/Router';
+import Route from './router/Route';
+import Root from './Page/Root';
+import About from './Page/About';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Router></Router>);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Router>
+    <Route path='/' component={<Root />} />
+    <Route path='/about' component={<About />} />
+  </Router>
+);
